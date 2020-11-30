9d42cbdb2f5fc821d2e8f9c363b533bcad95714f
Schascha <contact.schascha@gmail.com>
2020-11-30 19:20:54 +0000

    Deploying to gh-pages from  @ ca7ee633616ccf648af732ef8727d0a9dd1d4765 🚀

---
+++ precache-manifest.0c354c72b800148cd0ca541ebcc59796.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5dfd4988da5523293ac",
+    "url": "/makemake/css/app.374d5629.css"
+  },
+  {
+    "revision": "c3d3af3daec8a9637dc59013ef44b3d0",
+    "url": "/makemake/index.html"
+  },
+  {
+    "revision": "f5dfd4988da5523293ac",
+    "url": "/makemake/js/app.df20c944.js"
+  },
+  {
+    "revision": "d191b76b9d1bcc2fea67",
+    "url": "/makemake/js/chunk-vendors.462861c8.js"
+  },
+  {
+    "revision": "2540dc5c8a382846728edbdb4467fe20",
+    "url": "/makemake/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/makemake/robots.txt"
+  }
+]);
