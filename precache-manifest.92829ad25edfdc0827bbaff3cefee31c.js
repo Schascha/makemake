099987ca9a444032fe48802405288610c573d33d
Schascha <contact.schascha@gmail.com>
2021-03-17 12:39:01 +0000

    Deploying to gh-pages from  @ 8106a52c1b7e5a43a618cee20c092adcc05d8818 🚀

--- precache-manifest.92829ad25edfdc0827bbaff3cefee31c.js
+++ precache-manifest.92829ad25edfdc0827bbaff3cefee31c.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "61a95aede28cbe47dfcf",
+    "revision": "2e7a526016a35a7035a4",
     "url": "/makemake/css/app.f3b5503d.css"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/makemake/img/sharing.png"
   },
   {
-    "revision": "44e00c1f23e0ff40ff6edecfbc1977fd",
+    "revision": "5d9ff9554ca6e5b415be9b3212df1bcb",
     "url": "/makemake/index.html"
   },
   {
-    "revision": "61a95aede28cbe47dfcf",
-    "url": "/makemake/js/app.973b4366.js"
+    "revision": "2e7a526016a35a7035a4",
+    "url": "/makemake/js/app.bfba996b.js"
   },
   {
-    "revision": "e558d26b9e59a143435d",
-    "url": "/makemake/js/chunk-vendors.7b033440.js"
+    "revision": "da216dbbe8f9f5c9f26d",
+    "url": "/makemake/js/chunk-vendors.df7d6197.js"
   },
   {
     "revision": "3898e679f41de9a0f078845b98040077",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/makemake/precache-manifest.7eefe61e92c144a9de112e0c12472a95.js"
+  "/makemake/precache-manifest.92829ad25edfdc0827bbaff3cefee31c.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "makemake"});
